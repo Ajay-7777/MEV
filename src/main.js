9744b922afa85f0da4ac5fbcc81609ecae1de839
Ajay Panchal <ajaypanchal7778@gmail.com>
2022-12-03 04:36:48 +0530

    added README.md

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-const $ = require('jquery');
-// We declare it globally
-window.$ = $;
+// const $ = require('jquery');
+// // We declare it globally
+// window.$ = $;
 createApp(App).mount('#app')
--- vue.confi.js
+++ vue.confi.js
@@ -1,8 +1,8 @@
 module.exports ={
     devServer:{
         poroxy:{
-            '/api':{
-                target: 'http://localhost:3000',
+            '^/api':{
+                target: 'http://localhost:3080',
             },
         },
     },
